381b27d5d2c51a75e16d76d1d3eec44982cc083b
yashambodekar <144942722+yashambodekar@users.noreply.github.com>
2025-08-25 02:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,3 +82,9 @@ def recommend(user_input: Dict[str, Any], top_k: int = 3):
         "similar_users": top_users.tolist(),
         "recommendations": recs
     }
+
+   if __name__ == "__main__":
+    # Get the PORT from the environment variable provided by the platform (e.g., Render)
+    port = int(os.environ.get("PORT", 8000))
+    # '0.0.0.0' is the host that makes the app accessible from outside the container
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
